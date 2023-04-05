80bee924477ad1044ba4c7ef0e99669352d56e53
tizio-json <82601593+tizio-json@users.noreply.github.com>
2023-04-05 12:26:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 var nodes = []
 console.log(nodes)
+var NODI = 4;
+var MIN_HEART_RATE = 2_000;
+var MAX_HEART_RATE = 10_000;
+var T_FAIL = log2(NODI)*(MAX_HEART_RATE);
 
 class Node {
 
     container = document.getElementById("container");
+    
+    
 
     constructor(x, y, isFailed = false, color = "blue") {
         if (isFailed) {
@@ -13,10 +19,10 @@ class Node {
         }
         this.id = crypto.randomUUID();
         /* 
-         * this.heart_rate range => 500ms to 2000ms
+         * this.heart_rate range => 2_000ms to 10_000ms
          * (random_gen * (max - min)) + min
          */
-        this.heart_rate = (parseInt(Math.random() * (2000-500))) + 500;
+        this.heart_rate = (parseInt(Math.random() * (MAX_HEART_RATE-MIN_HEART_RATE))) + 2_000;
         this.freq_rate = parseFloat(((1/this.heart_rate)*100).toFixed(3));
         this.x = parseInt(x);
         this.y = parseInt(y);
@@ -26,15 +32,19 @@ class Node {
         this.children = [];
         this.probVal = parseFloat((1/(Math.random()*(20-10)+10)).toFixed(2));
         this.probMatrix = [[(1 - this.probVal).toFixed(2), this.probVal.toFixed(2)], [0, 1]];
-        // this.comunication_data = {}
+        this.comunication_data = {}
         this.other_nodes_data = {}
         this.other_nodes_data[this.id] = [this.isFailed, getDate()] // backup object for dead nodes
+        // this.comunication_data[this.id] = `info su ${this.id}`
         this.listeners = {};
         this.create();
         this.updateNodesList()
+        // this.failTimer();
         this.send_data();
         this.initNode();
+        this.T_Fail_check()
     }
+    
 
     addEventListener(eventName, callback) {
         if (!this.listeners[eventName]) {
@@ -49,10 +59,10 @@ class Node {
         }
       }
     
-      emit(eventName, data) {
+      emit(eventName, data_nodes, comunication_data={}) {
         Object.values(nodes).forEach(node => {
           if (node.listeners[eventName]) {
-            node.listeners[eventName].forEach(callback => callback(data));
+            node.listeners[eventName].forEach(callback => callback(data_nodes,comunication_data));
           }
         });
       }
@@ -74,8 +84,84 @@ class Node {
         }
     }
 
+    T_Fail_check(){
+
+        var interval_id = setInterval(() => { 
+            return new Promise(resolve => {
+                
+                resolve(() => {   
+                    
+                    if (!(this.isFailed)) {
+                        this.T_Fail_action();
+                    }
+                        
+                    else {
+                        clearInterval(interval_id);
+                    }
+                });
+            }).then(callback => callback());
+        }, T_FAIL);
+
+        /*
+        var interval_id = setInterval(async () => { 
+            const callback = await new Promise(resolve => {
+
+                resolve(() => {
+
+                    if (!(this.isFailed)) {
+                        this.T_Fail_action();
+                    }
+                        
+                    else {
+                        clearInterval(interval_id);
+                    }
+                });
+            });
+            return callback();
+        }, T_FAIL);
+        */
+    }
+
+
+    T_Fail_action() {
+        console.log("TFAIL_ACTION");
+        if (this.other_nodes_data) {
+            for (let objKey in this.other_nodes_data) {
+                let obj = this.other_nodes_data[objKey];
+                // console.log(obj[1])
+                // console.log(getDate())
+                // console.log(getDate() - obj[1])
+                // console.log(!((getDate() - obj[1]) <= T_FAIL))
+                if (!((getDate() - obj[1]) <= T_FAIL)) {
+                    // console.log(obj);
+                    // console.log(obj[1]);
+                    console.log(`id: ${this.id}; id_eliminato: ${objKey}`)
+                    delete this.other_nodes_data[objKey];
+                }
+            }
+        }
+    }
+
+    // T_Fail_action(){
+    //     console.log("TFAIL_ACTION")
+    //     if(this.other_nodes_data){
+    //         for (let info in this.other_nodes_data) {
+    //             for (let key in info){
+    //                 console.log(info[key][1])
+    //                 console.log(getDate())
+    //                 console.log(getDate() - info[key][1])
+    //                 if(!((getDate() - info[key][1]) >= T_FAIL)){
+    //                     console.log(`${info}: ${info[key]}`)
+    //                     delete info[key];
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
     initNode(){
-        this.addEventListener(this.id, data => {
+
+        this.addEventListener(this.id, (data, comunication_data) => {
             // console.log(`Nodo: ${this.id}; data: ${data}`);
             for (let key in data) {
                 if (key in this.other_nodes_data) {
@@ -86,6 +172,12 @@ class Node {
                   this.other_nodes_data[key] = data[key];
                 }
               }
+            // console.log(comunication_data)
+            for (let key in comunication_data) {
+                if (!this.comunication_data.hasOwnProperty(key)) {
+                    this.comunication_data[key] = comunication_data[key];
+                }
+            }
           });
         // this.comunication_data = {id: this.id, date: getDate()}
     }
@@ -98,14 +190,14 @@ class Node {
         el.style.bottom = `${this.y}px`;
         el.setAttribute("data-id", this.id)
         container.appendChild(el)
-        var circle = document.createElement("div");
-        circle.classList.add("circle_range");
-        circle.style.width = `${this.range}px`;      
-        circle.style.height = `${this.range}px`;      
-        circle.style.left = `${this.x}px`;      
-        circle.style.bottom = `${this.y}px`;   
-        el.setAttribute("data-node-id", this.id)
-        container.appendChild(circle)
+        // var circle = document.createElement("div");
+        // // circle.classList.add("circle_range");
+        // circle.style.width = `${this.range}px`;      
+        // circle.style.height = `${this.range}px`;      
+        // circle.style.left = `${this.x}px`;      
+        // circle.style.bottom = `${this.y}px`;   
+        // el.setAttribute("data-node-id", this.id)
+        // container.appendChild(circle)
     }
 
     vis_send_data(){
@@ -120,7 +212,6 @@ class Node {
         
         // setTimeout(() => {
         //     document.getElementsByClassName("circle")[0].remove();
-        //     document.getElementsByClassName("circle-border")[0].remove();
         // }, 2*1000)
       
         setTimeout(() => {
@@ -142,7 +233,10 @@ class Node {
     }
     prepareData(){
         var dest_nodes = [] // nodi destinatari
-        var index1 = Math.floor((Math.random() * 1000)%nodes.length);
+        do{
+            var index1 = Math.floor((Math.random() * 1000)%nodes.length);
+        } while (nodes[index1] === this);
+
         dest_nodes.push(nodes[index1]);
 
         var index2;
@@ -151,33 +245,70 @@ class Node {
         do {
             index2 = Math.floor(Math.random() * nodes.length);
             elemento2 = nodes[index2];
-        } while (index2 === index1);
+        } while (index2 === index1 && nodes[index2] === this);
+
         dest_nodes.push(elemento2);
         return dest_nodes;
     }
     
-    async send_data(){
-        var intervalID = setInterval(() => { 
-            return new Promise(resolve => {
-                resolve(() => {   
-                    if(!(this.isFailed)){
-                        if (eseguiConProb(this.probVal)){
-                            this.vis_send_data();
-                            /* send data */
-                            this.prepareData().forEach((node_dest) => {
-                                this.other_nodes_data[this.id][1] = getDate();
-                                this.emit(node_dest.id, this.other_nodes_data);
-                            })                        
+    async send_data(comunication_data={}){
+
+            if (Object.keys(this.comunication_data).length != 0){
+                comunication_data=this.comunication_data
+
+            }
+
+            var interval_id = setInterval(() => { 
+                return new Promise(resolve => {
+                    
+                    resolve(() => {   
+                        
+                        if(!(this.isFailed)){
+                            if (eseguiConProb(this.probVal)){
+                                this.vis_send_data();
+                                /* send data */
+                                this.prepareData().forEach((node_dest) => {
+                                    this.other_nodes_data[this.id][1] = getDate();
+                                    
+                                    this.emit(node_dest.id, this.other_nodes_data, comunication_data);
+                                })                        
+                            }
+                            else{
+                                this.changeStatus = true;
+                                console.log(`${this.id} fallito`)
+                                clearInterval(interval_id);
+                            }
                         }
-                        else{
-                            this.changeStatus = true;
-                            console.log(`${this.id} fallito`);
-                            clearInterval(intervalID);
+                    });
+                }).then(callback => callback());
+            }, this.heart_rate); 
+
+            /*
+                var interval_id = setInterval(async () => { 
+                const callback = await new Promise(resolve => {
+
+                    resolve(() => {
+
+                        if (!(this.isFailed)) {
+                            if (eseguiConProb(this.probVal)) {
+                                this.vis_send_data();
+                                this.prepareData().forEach((node_dest) => {
+                                    this.other_nodes_data[this.id][1] = getDate();
+
+                                    this.emit(node_dest.id, this.other_nodes_data, comunication_data);
+                                });
+                            }
+                            else {
+                                this.changeStatus = true;
+                                console.log(`${this.id} fallito`);
+                                clearInterval(interval_id);
+                            }
                         }
-                    }
+                    });
                 });
-            }).then(callback => callback());
-        }, this.heart_rate); 
+                return callback();
+            }, this.heart_rate);
+            */
 
     }
     /*
@@ -196,7 +327,7 @@ class Node {
     */
 
     async failTimer() {
-        // randomTime range => 5000ms to 30_000ms
+        // randomTime range => 5_000ms to 30_000ms
         const randomTime = (Math.random() * (30_000-5000)) + 5000;
         console.log(randomTime)
 
@@ -214,16 +345,21 @@ class Node {
     }
 }
 
-// width_range = getComputedStyle(document.documentElement).getPropertyValue('--container-w')
-// height_range = getComputedStyle(document.documentElement).getPropertyValue('--container-h')
+function log2(val) {
+    return Math.ceil(Math.log(val) / Math.log(2));
+}
 
-for(i=0; i<5; i++){
+width_range = getComputedStyle(document.documentElement).getPropertyValue('--container-w')
+height_range = getComputedStyle(document.documentElement).getPropertyValue('--container-h')
+
+for(i=0; i<NODI; i++){
     new Node((Math.random()*1190)+10, (Math.random()*890)+10)
 }
 
-console.log(nodes)
+// console.log(nodes)
 max_heartRate = nodes.map(node => node.heart_rate).reduce((a, b) => Math.max(a,b))
-console.log(max_heartRate)
+// console.log(max_heartRate)
+
 ordered_nodes = nodes.sort((a, b) => a.heart_rate - b.heart_rate);
 console.log(ordered_nodes)
 
@@ -238,7 +374,7 @@ console.table(filteredArray)
 function eseguiConProb(failPRob) {
     return !(Math.random() <= failPRob);
     /*
-    if (Math.random() <= failPRob) {
+    if (Math.random() < failPRob) {
         // Esegui la funzione
         console.log("La funzione è stata eseguita con successo.");
     } else {
@@ -257,26 +393,4 @@ function getDate(){
     let seconds = String(date.getSeconds()).padStart(2, '0')
     return date
 }
-
-genForm = document.getElementById("new-values");
-genForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    /*
-    container = document.getElementById("container");
-    container.remove();
-    newContainer = document.createElement("div");
-    newContainer.setAttribute("id", "container");
-    document.body.insertBefore(newContainer, genForm);
-    nodes.forEach((node) => {
-        delete node;
-    })
-    nodes = []
-    nInput = document.getElementById("n");
-    setTimeout(() => {
-        for(i=0; i<parseInt(nInput.value); i++){
-            new Node((Math.random()*1190)+10, (Math.random()*890)+10)
-        }
-    }, 1000)
-    */
-});
 
